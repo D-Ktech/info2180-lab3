ba4fc6e95cb8dd69b8d7888e4d796dd1594eab67
Dravon Anderson <dravonanderson75@gmail.com>
2020-10-13 16:07:53 -0500

    added X and O marks

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,4 +5,34 @@ window.onload = function(){
   for (var n = 0; n < x.length; n++){
     x[n].className = "square";
   }
+
+  let circleTurn;
+  var X_CLASS = 'X';
+  var O_CLASS = 'O';
+  
+  var squares = document.querySelectorAll('#board div');
+
+
+  squares.forEach(square=>{
+    square.addEventListener('click',handleClick, {once: true})
+  })
+
+  function handleClick(s){
+    var square = s.target;
+    var currentClass = circleTurn ? O_CLASS : X_CLASS;
+    placeMark(square,currentClass)
+    swapTurns()
+    console.log('clicked')
+  }
+
+  function placeMark(square, currentClass){
+   square.classList.add(currentClass)
+   console.log(square, currentClass)
+  }
+
+  function swapTurns(){
+    circleTurn = !circleTurn
+  }
+
 }
+
